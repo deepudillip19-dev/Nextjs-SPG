d5337a5b3803bc22bab9b7dc79cdb577e5b8d79e
deepudillip19-dev <deepu.dillip19@gmail.com>
2026-06-22 18:21:40 +0530

    Add GitHub push instructions to MyAccount.tsx
    Add instructions for preparing and pushing changes to GitHub.

---
+++ src/components/MyAccount.tsx
@@ -0,0 +1,12 @@
+# 1. Prepare your changes
+git add .
+
+# 2. Save your changes locally
+git commit -m "Fix MyAccount UI and link GitHub repo"
+
+# 3. Connect to your specific repository
+# (Run this even if you did it before to make sure it's correct)
+git remote set-url origin https://github.com/deepudillip19-dev/Nextjs-SPG.git
+
+# 4. Push the code to GitHub
+git push -u origin main
